55e35dcbc6812e203e24c9381cd2838954910e06
R3A2 <42949334+R3A2@users.noreply.github.com>
2018-09-09 15:02:25 +0100

    Update Divine-Bot.py

--- Divine-Bot.py
+++ Divine-Bot.py
@@ -14,7 +14,7 @@ bot.remove_command("help")
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(game=discord.Game(name="with commands!"))
+    await bot.change_presence(game=discord.Game(name="Type !Help"))
     print ("Ready when you are.")
     print ("I am running on " + bot.user.name)
     print ("With the ID: " + bot.user.id)
